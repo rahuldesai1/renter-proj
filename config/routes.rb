3e65955c3a5c9780d2088383591f26929b476af8
rahuldesai1 <rahuldesai@berkeley.edu>
2018-11-15 18:45:29 -0800

    Created routes for all the pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   root to: "home#index", as: "home"
+  get "/product/details", to: "products#index", as: "product"
+  get "/product/review/new", to: "reviews#index", as: "review"
+  get "/product/new", to: "products#new", as: "product_new"
+  get "/transaction", to: "transactions#index", as: "transaction"
 end
