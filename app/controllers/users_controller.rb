bcfec45222f7214e2de7d5a041dba1b3f38a3563
Anjali Kantharuban <anjali@airbears2-10-142-25-189.airbears2.1918.berkeley.edu>
2018-11-17 20:04:14 -0800

    Profile Halfway Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-	
-	before_action :authenticate_user!
-	
-	def index
 
+	before_action :authenticate_user!
+
+	def index
+		@products = Product.find_by_owner_id(current_user)
+		@transactions = Transaction.find_by_renter_id(current_user)
 	end
 
 
