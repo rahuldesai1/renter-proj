d9e7154be1b04200573c27092d930f16f7e6e99e
Sophia Ding <sophiading@berkeley.edu>
2018-11-25 18:09:35 -0800

    photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_033527) do
+ActiveRecord::Schema.define(version: 2018_11_18_040959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_11_14_033527) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "img"
     t.index ["owner_id"], name: "index_products_on_owner_id"
   end
 
